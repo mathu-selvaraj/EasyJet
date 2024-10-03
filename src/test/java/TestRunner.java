7bf383261b35bc1aa2bfc9b5b45fa127301eca6b
mathu-selvaraj <Mathubala.Selvaraj@easyJet.com>
2024-10-03 12:25:10 +0530

    Create TestRunner.java

---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,13 @@
+// src/test/java/TestRunner.java
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/resources/features",
+    glue = "stepdefinitions"
+)
+public class TestRunner {
+}
